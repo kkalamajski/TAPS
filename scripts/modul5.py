727eab20fec0ba35e36ce04dc6987dae3cfffabe
Karol Kałamajski <195609@edu.p.lodz.pl>
2020-09-03 23:35:00 +0200

    postepy przy pracy domowej modul 5

--- scripts/modul5.py
+++ scripts/modul5.py
@@ -1,11 +1,18 @@
 import requests
+import json
 
+def stats(function):
+    def inside():
+        function()
+    return inside()
+
+@stats
 def exchange_rate():
     try:
-        query = {'base':'USD','symbols':'PLN'}
+        query = {'base': 'USD', 'symbols': 'PLN'}
         r = requests.get('https://api.exchangeratesapi.io/latest', params=query)
-        print(r.text)
-    except TimeoutError:
+        response_dictionary = json.loads(r.content)
+        rates = response_dictionary['rates']
+        print('Kurs dolara: ', rates['PLN'])
+    except requests.exceptions.ConnectTimeout:
         print('Timeout')
-
-exchange_rate()
